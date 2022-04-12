c4795525ee4eb8e5bb20db605ac092c6917ecb94
Edinh <edinhurem@gmail.com>
2022-04-12 17:27:04 -0400

    update

--- index.js
+++ index.js
@@ -132,74 +132,74 @@ function showMenu() {
 
 // To create a function....
 /*
-function function_name(parameters) {   // "parameters" are optional information passed to the function
+    function function_name(parameters) {   // "parameters" are optional information passed to the function
 
-  ...
-}
-*/
+      ...
+    }
+    */
 
 function createHTML() {
   // Create the html file for the web page: index.html
 
   // Creating the html header informtion
   let html = `
-  <!DOCTYPE html>
-  <html>
-  <head>
-    <link rel="stylesheet" href="styles.css">
+      <!DOCTYPE html>
+      <html>
+      <head>
+        <link rel="stylesheet" href="styles.css">
 
-    <link href="./font-awesome/all.min.css" rel="stylesheet">
-    <script src="./font-awesome/all.min.js"></script>
+        <link href="./font-awesome/all.min.css" rel="stylesheet">
+        <script src="./font-awesome/all.min.js"></script>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-  </head>
-  <body>
-  <header class="team-profile-header">
-    ~ My Team Profile ~
-  </header>
-  `;
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+      </head>
+      <body>
+      <header class="team-profile-header">
+        ~ My Team Profile ~
+      </header>
+      `;
 
   // Add the manager to the page
   html += `
-  <div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">${manager.getName()}</h5>
-    <h5 class="card-title"><i class="fa-solid fa-people-roof"></i> ${manager.getRole()}</h5>
-    <p class="card-text">ID: ${manager.getId()}</p>
-    <p class="card-text">E-Mail: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
-    <p class="card-text">Office #: ${manager.getOfficeNumber()}</p>
-  </div>
-</div>`;
+      <div class="card" style="width: 18rem;">
+      <div class="card-body">
+        <h5 class="card-title">${manager.getName()}</h5>
+        <h5 class="card-title"><i class="fa-solid fa-people-roof"></i> ${manager.getRole()}</h5>
+        <p class="card-text">ID: ${manager.getId()}</p>
+        <p class="card-text">E-Mail: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
+        <p class="card-text">Office #: ${manager.getOfficeNumber()}</p>
+      </div>
+    </div>`;
 
   // Add the engineers to the page
   for (e of engineers) {
     html += `
-    <div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">${e.getName()}</h5>
-      <h5 class="card-title"><i class="fa-solid fa-computer"></i> ${e.getRole()}</h5>
-      <p class="card-text">ID: ${e.getId()}</p>
-      <p class="card-text">E-Mail: <a href="mailto:${e.getEmail()}">${e.getEmail()}</a></p>
-      <p class="card-text">Github: <a href="https://github.com/${e.getGithub()}" target="_blank">${e.getGithub()}</a></p>
-    </div>
-  </div>`;
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${e.getName()}</h5>
+          <h5 class="card-title"><i class="fa-solid fa-computer"></i> ${e.getRole()}</h5>
+          <p class="card-text">ID: ${e.getId()}</p>
+          <p class="card-text">E-Mail: <a href="mailto:${e.getEmail()}">${e.getEmail()}</a></p>
+          <p class="card-text">Github: <a href="https://github.com/${e.getGithub()}" target="_blank">${e.getGithub()}</a></p>
+        </div>
+      </div>`;
   }
 
   // Add the interns to the page
 
   for (i of interns) {
     html += `
-    <div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">${i.getName()}</h5>
-      <h5 class="card-title"><i class="fa-solid fa-user-graduate"></i> ${i.getRole()}</h5>
-      <p class="card-text">ID: ${i.getId()}</p>
-      <p class="card-text">E-Mail: <a href="mailto:${i.getEmail()}">${i.getEmail()}</a></p>
-      <p class="card-text">School: ${i.getSchool()}</p>
-    </div>
-  </div>`;
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h5 class="card-title">${i.getName()}</h5>
+          <h5 class="card-title"><i class="fa-solid fa-user-graduate"></i> ${i.getRole()}</h5>
+          <p class="card-text">ID: ${i.getId()}</p>
+          <p class="card-text">E-Mail: <a href="mailto:${i.getEmail()}">${i.getEmail()}</a></p>
+          <p class="card-text">School: ${i.getSchool()}</p>
+        </div>
+      </div>`;
   }
 
   // Add the footer html
@@ -234,9 +234,9 @@ function createHTML() {
  * This function will generate test data quickly skipping the questionaire for generation
  */
 function generateQuickTest() {
-  manager = new Manager("M001", "edin hurem", "edinhurem@gmail.com", "1");
-  engineers.push(new Engineer("E001", "edin", "edin@company.com", "edinhurem"));
-  interns.push(new Intern("John", "I001", "JohnUNCC@gmail.com", "UNCC"));
+  manager = new Manager("jared doe", "M001", "jared.doe@gmail.com", "1");
+  engineers.push(new Engineer("sam doe", "2", "sam.doe@company.com", "samdoe"));
+  interns.push(new Intern("john doe", "5", "john.doe@gmail.com", "UNCC"));
   createHTML();
 }
 
