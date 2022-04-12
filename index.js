ffb305815295769c56eb190061992924b327de7a
Edinh <edinhurem@gmail.com>
2022-04-11 20:48:34 -0400

    processing

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
-const fs = require("fs");
+const fs = require("fs-extra");
+
+//hold our data here as a local database
+let manager;
+let engineers = [];
+let interns = [];
 
 // Get the information for the manager, then create the manager object
 // get name, id, and email from user
-
 function createManger() {
   inquirer
     .prompt([
@@ -142,22 +146,30 @@ function createHTML() {
   <!DOCTYPE html>
   <html>
   <head>
-  
+    <link rel="stylesheet" href="styles.css">
+
+    <link href="./font-awesome/all.min.css" rel="stylesheet">
+    <script src="./font-awesome/all.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
   </head>
   <body>
+  <header class="team-profile-header">
+    ~ My Team Profile ~
+  </header>
   `;
 
   // Add the manager to the page
-
   html += `
   <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="..." alt="Card image cap">
   <div class="card-body">
-  <h5 class="card-title">${manager.getName()}</h5>
-  <h5 class="card-title">${manager.getRole()}</h5>
-  <p class="card-text">ID: ${manager.getId()}</p>
-  <p class="card-text">E-Mail:<a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
-  <p class="card-text">Office: ${manager.getOfficeNumber()}</p>
+    <h5 class="card-title">${manager.getName()}</h5>
+    <h5 class="card-title"><i class="fa-solid fa-people-roof"></i> ${manager.getRole()}</h5>
+    <p class="card-text">ID: ${manager.getId()}</p>
+    <p class="card-text">E-Mail: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></p>
+    <p class="card-text">Office #: ${manager.getOfficeNumber()}</p>
   </div>
 </div>`;
 
@@ -165,13 +177,12 @@ function createHTML() {
   for (e of engineers) {
     html += `
     <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="..." alt="Card image cap">
     <div class="card-body">
-    <h5 class="card-title">${e.getName()}</h5>
-    <h5 class="card-title">${e.getRole()}</h5>
-    <p class="card-text">ID: ${e.getId()}</p>
-    <p class="card-text">E-Mail:<a href="mailto:${e.getEmail()}">${e.getEmail()}</a></p>
-    <p class="card-text">Github: ${e.getGithub()}</p>
+      <h5 class="card-title">${e.getName()}</h5>
+      <h5 class="card-title"><i class="fa-solid fa-computer"></i> ${e.getRole()}</h5>
+      <p class="card-text">ID: ${e.getId()}</p>
+      <p class="card-text">E-Mail: <a href="mailto:${e.getEmail()}">${e.getEmail()}</a></p>
+      <p class="card-text">Github: <a href="https://github.com/${e.getGithub()}" target="_blank">${e.getGithub()}</a></p>
     </div>
   </div>`;
   }
@@ -181,13 +192,12 @@ function createHTML() {
   for (i of interns) {
     html += `
     <div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="..." alt="Card image cap">
     <div class="card-body">
-    <h5 class="card-title">${i.getName()}</h5>
-    <h5 class="card-title">${i.getRole()}</h5>
-    <p class="card-text">ID: ${i.getId()}</p>
-    <p class="card-text">E-Mail:<a href="mailto:${i.getEmail()}">${i.getEmail()}</a></p>
-    <p class="card-text">Github: ${i.getSchool()}</p>
+      <h5 class="card-title">${i.getName()}</h5>
+      <h5 class="card-title"><i class="fa-solid fa-user-graduate"></i> ${i.getRole()}</h5>
+      <p class="card-text">ID: ${i.getId()}</p>
+      <p class="card-text">E-Mail: <a href="mailto:${i.getEmail()}">${i.getEmail()}</a></p>
+      <p class="card-text">School: ${i.getSchool()}</p>
     </div>
   </div>`;
   }
@@ -202,10 +212,43 @@ function createHTML() {
       console.log("The html file has been created.");
     }
   });
+
+  //copy our styles.css file to the dist folder
+  fs.copyFile("./src/styles.css", "./dist/styles.css", (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("The css was copied to dist folder.");
+    }
+  });
+
+  //copy our font-awesome to the dist folder
+  fs.copy("./src/font-awesome", "./dist/font-awesome", (err) => {
+    if (err) console.log(err);
+    else {
+      console.log("Font-awesome was copied to dist folder.");
+    }
+  });
 }
 
-let manager;
-let engineers = [];
-let interns = [];
+/**
+ * This function will generate test data quickly skipping the questionaire for generation
+ */
+function generateQuickTest() {
+  manager = new Manager(
+    "M001",
+    "edin hure",
+    "edinhurem@gmail.com",
+    "Charlotte"
+  );
+  engineers.push(new Engineer("E001", "Edin", "edin@company.com", "edinhurem"));
+  interns.push(
+    new Intern("Stephanie", "I001", "stephiscool@gmail.com", "CCNV")
+  );
+  createHTML();
+}
 
+//use this to test the html generation quickly
+//generateQuickTest();
+
+//this starts the questionaire for generation
 createManger();
